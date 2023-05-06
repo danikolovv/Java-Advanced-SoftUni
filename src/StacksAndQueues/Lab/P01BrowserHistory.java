93ce0d8f5f4e3acef7947f5eec2065c94fe43c20
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-06 21:26:23 +0300

    Initial commit

---
+++ src/StacksAndQueues/Lab/P01BrowserHistory.java
@@ -0,0 +1,11 @@
+package StacksAndQueues.Lab;
+
+import java.util.Scanner;
+
+public class P01BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
