58ddc388b4634cc49c592810a6d37210aa3e3b0e
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-19 22:31:40 +0300

    Initial commit

---
+++ src/StacksAndQueues/Lab/P03DecimalToBinaryConverter.java
@@ -0,0 +1,16 @@
+package StacksAndQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P03DecimalToBinaryConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+
+    }
+}
