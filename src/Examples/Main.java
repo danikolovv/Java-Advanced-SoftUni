bcac785d4201ab1fbaf1fdcc12938736916e9b44
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-19 14:09:47 +0300

    Initial commit

---
+++ src/Examples/Main.java
@@ -0,0 +1,21 @@
+package Examples;
+
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        stack.push(92);
+        stack.push(81);
+        stack.push(37);
+        stack.push(45);
+        stack.push(12);
+
+        while (!stack.isEmpty()) {
+            int topElement = stack.pop();
+            System.out.println(topElement);
+        }
+    }
+}
