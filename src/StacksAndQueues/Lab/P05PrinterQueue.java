d4dc28763880ffa36ae3b09a14b7fc9bb7c2f270
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-21 19:21:36 +0300

    Initial commit

--- src/StacksAndQueues/Lab/P05PrinterQueue.java
+++ src/StacksAndQueues/Lab/P05PrinterQueue.java
@@ -1,11 +1,33 @@
 package StacksAndQueues.Lab;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class P05PrinterQueue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String fileName  = scanner.nextLine();
 
+        ArrayDeque<String> queue = new ArrayDeque<>();
+
+        while (!fileName.equals("print")){
+
+            if (fileName.equals("cancel")){
+                if (!queue.isEmpty()){
+                    System.out.println("Canceled " + queue.poll());
+                } else {
+                    System.out.println("Printer is on standby");
+                }
+            } else {
+                queue.offer(fileName);
+            }
+
+            fileName = scanner.nextLine();
+        }
+
+        while (!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
     }
 }
