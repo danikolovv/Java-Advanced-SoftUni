56e7f380a5d7adb449ad89327b5c069fb2a3c48e
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-21 18:51:01 +0300

    Initial commit

---
+++ src/StacksAndQueues/Lab/P04MatchingBrackets.java
@@ -0,0 +1,26 @@
+package StacksAndQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P04MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String expression = scanner.nextLine();
+
+        ArrayDeque<Integer> openIndexes = new ArrayDeque<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char current = expression.charAt(i);
+
+            if (current == '('){
+                openIndexes.push(i);
+            } else if (current == ')'){
+                int lastOpenIndex = openIndexes.pop();
+                String subExpression = expression.substring(lastOpenIndex, i + 1);
+                System.out.println(subExpression);
+            }
+        }
+    }
+}
