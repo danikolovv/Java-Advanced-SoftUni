897817f86d307f9651c2066dbada4cb5c31aca7d
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-21 19:04:38 +0300

    Initial commit

---
+++ src/StacksAndQueues/Lab/P05PrinterQueue.java
@@ -0,0 +1,11 @@
+package StacksAndQueues.Lab;
+
+import java.util.Scanner;
+
+public class P05PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
