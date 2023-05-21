ccecf332c5064e47975559b495a146b485cf7431
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-21 20:56:00 +0300

    Initial commit

--- src/StacksAndQueues/Lab/P07MathPotato.java
+++ src/StacksAndQueues/Lab/P07MathPotato.java
@@ -1,11 +1,51 @@
 package StacksAndQueues.Lab;
 
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class P07MathPotato {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] names = scanner.nextLine().split("\\s+");
+        int n = scanner.nextInt();
 
+        PriorityQueue<String> queue = new PriorityQueue<>();
+
+        int cycle = 1;
+
+        for (String name : names){
+            queue.offer(name);
+        }
+
+        while (queue.size() > 1) {
+            for (int i = 1; i < n; i++) {
+                queue.offer(queue.poll());
+            }
+
+
+            if (isPrime(cycle)) {
+                System.out.println("Prime " + queue.peek());
+            } else {
+                System.out.println("Removed " + queue.poll());
+            }
+            cycle++;
+        }
+        System.out.println("Last is " + queue.poll());
+    }
+
+    private static boolean isPrime(int cycle) {
+        if (cycle <= 1){
+            return false;
+        }
+
+        for (int i = 2; i < cycle; i++) {
+            if (cycle % i == 0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
