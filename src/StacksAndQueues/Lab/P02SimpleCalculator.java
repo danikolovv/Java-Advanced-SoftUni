5d05b4a5f0e5b99dad9031ad509e833fe7bfd35b
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-21 18:36:40 +0300

    Initial commit

--- src/StacksAndQueues/Lab/P02SimpleCalculator.java
+++ src/StacksAndQueues/Lab/P02SimpleCalculator.java
@@ -1,4 +1,33 @@
 package StacksAndQueues.Lab;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class P02SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] inputArr = input.split(" ");
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        String operator = null;
+        for (String item : inputArr){
+
+            if (item.equals("+") || item.equals("-")){
+                operator = item;
+            } else if (stack.isEmpty()){
+                stack.push(Integer.parseInt(item));
+            } else {
+                int currentNumber = Integer.parseInt(item);
+                int lasNumber = stack.peek();
+                stack.pop();
+                if (operator.equals("+")){
+                    stack.push(currentNumber + lasNumber);
+                } else {
+                    stack.push(lasNumber - currentNumber);
+                }
+            }
+        }
+        System.out.println(stack.peek());
+    }
 }
