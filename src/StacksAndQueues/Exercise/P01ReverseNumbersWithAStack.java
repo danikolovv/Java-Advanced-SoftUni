98e0391754bcacaf782635e61ae4229080f6439d
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-21 21:26:13 +0300

    Initial commit

---
+++ src/StacksAndQueues/Exercise/P01ReverseNumbersWithAStack.java
@@ -0,0 +1,11 @@
+package StacksAndQueues.Exercise;
+
+import java.util.Scanner;
+
+public class P01ReverseNumbersWithAStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
