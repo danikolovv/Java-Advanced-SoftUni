6cb64e5d758692b209cba079f7a21ce622b88c9b
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-23 18:53:29 +0300

    Initial commit

--- src/StacksAndQueues/Exercise/P01ReverseNumbersWithAStack.java
+++ src/StacksAndQueues/Exercise/P01ReverseNumbersWithAStack.java
@@ -9,15 +9,16 @@ public class P01ReverseNumbersWithAStack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
-                            .mapToInt(Integer::parseInt)
-                            .toArray();
-
         ArrayDeque<Integer> stack = new ArrayDeque<>();
 
-        for (int number : numbers){
-            stack.push(number);
-        }
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                            .mapToInt(Integer::parseInt)
+                            .forEach(stack::push);
+
+
+//        for (int number : numbers){
+//            stack.push(number);
+//        }
 
         for (Integer integer : stack) {
             System.out.print(stack.pop() + " ");
