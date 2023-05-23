8316b8513d358aa5db5e4a4c4936a7928298f57d
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-23 18:50:23 +0300

    Initial commit

--- src/StacksAndQueues/Exercise/P01ReverseNumbersWithAStack.java
+++ src/StacksAndQueues/Exercise/P01ReverseNumbersWithAStack.java
@@ -1,12 +1,30 @@
 package StacksAndQueues.Exercise;
 
+import java.lang.reflect.Array;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P01ReverseNumbersWithAStack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                            .mapToInt(Integer::parseInt)
+                            .toArray();
 
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int number : numbers){
+            stack.push(number);
+        }
+
+        for (Integer integer : stack) {
+            System.out.print(stack.pop() + " ");
+        }
+
+//        while (!stack.isEmpty()){
+//            System.out.print(stack.pop() + " ");
+//        }
     }
 }
---
+++ src/StacksAndQueues/Exercise/P02BasicStackOperations.java
@@ -0,0 +1,11 @@
+package StacksAndQueues.Exercise;
+
+import java.util.Scanner;
+
+public class P02BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
