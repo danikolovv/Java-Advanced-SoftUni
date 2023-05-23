5165668233a65dea0f3daed778260b9fd6afe487
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-23 22:50:20 +0300

    Initial commit

--- src/StacksAndQueues/Exercise/P06RecursiveFibonacci.java
+++ src/StacksAndQueues/Exercise/P06RecursiveFibonacci.java
@@ -3,11 +3,13 @@ package StacksAndQueues.Exercise;
 import java.util.Scanner;
 
 public class P06RecursiveFibonacci {
+    private static long[] memory;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
+        memory = new long[n + 1];
         long result = fib(n);
         System.out.println(result);
     }
@@ -16,7 +18,10 @@ public class P06RecursiveFibonacci {
         if (n < 2) {
             return 1;
         }
-
-        return fib(n - 1) + fib(n - 2);
+        if (memory[n] != 0) {
+            return memory[n];
+        }
+        memory[n] = fib(n - 1) + fib(n - 2);
+        return memory[n];
     }
 }
