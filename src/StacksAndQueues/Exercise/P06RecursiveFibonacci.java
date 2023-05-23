6fa663e9ead3145a35998436d53f97f7b9317def
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-23 22:43:12 +0300

    Initial commit

--- src/StacksAndQueues/Exercise/P06RecursiveFibonacci.java
+++ src/StacksAndQueues/Exercise/P06RecursiveFibonacci.java
@@ -6,6 +6,17 @@ public class P06RecursiveFibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        long result = fib(n);
+        System.out.println(result);
+    }
+
+    private static long fib(int n) {
+        if (n < 2) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
     }
 }
