93043471f5e6983e5aa01f105be0f7b4a1b53297
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-05-28 00:18:44 +0300

    Initial commit

--- src/SetsAndMapsAdvanced/Lab/P01ParkingLot.java
+++ src/SetsAndMapsAdvanced/Lab/P01ParkingLot.java
@@ -1,11 +1,37 @@
 package SetsAndMapsAdvanced.Lab;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class P01ParkingLot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
 
+        Set<String> carNumbers = new LinkedHashSet<>();
+
+        while (!input.equals("END")) {
+            String[] tokens = input.split(", ");
+            String directions = tokens[0];
+            String carRegistration = tokens[1];
+
+            if (directions.equals("IN")) {
+                carNumbers.add(carRegistration);
+            } else {
+                carNumbers.remove(carRegistration);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        if (carNumbers.isEmpty()) {
+            System.out.println("Parking Lot is Empty");
+        } else {
+            for (String carNumber : carNumbers) {
+                System.out.println(carNumber);
+            }
+        }
     }
 }
