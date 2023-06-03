f51da131eab1e8bea75bd2dc339e677c01266f13
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-06-03 21:55:43 +0300

    Initial commit

--- src/Examples/ExampleOfMap.java
+++ src/Examples/ExampleOfMap.java
@@ -8,27 +8,6 @@ public class ExampleOfMap {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, Integer> population = new HashMap<>();
 
-        population.put("Sofia", 1200000);
-        population.put("Plovdiv", 700000);
-        population.put("Varna", 600000);
-        population.put("Burgas", 500000);
-
-        System.out.println(population.get("Sofia"));
-
-        System.out.println("===");
-
-        for (Map.Entry<String, Integer> entry : population.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
-
-        population.clear();
-
-        population.putIfAbsent(scanner.nextLine(), Integer.parseInt(scanner.nextLine()));
-
-        for (Map.Entry<String, Integer> entry : population.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
     }
 }
