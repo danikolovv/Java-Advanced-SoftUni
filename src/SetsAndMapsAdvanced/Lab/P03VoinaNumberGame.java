625f4e3699a86e260fee09cda5c1693ac5f2f587
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-06-03 23:39:23 +0300

    Initial commit

--- src/SetsAndMapsAdvanced/Lab/P03VoinaNumberGame.java
+++ src/SetsAndMapsAdvanced/Lab/P03VoinaNumberGame.java
@@ -1,11 +1,60 @@
 package SetsAndMapsAdvanced.Lab;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class P03VoinaNumberGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Set<Integer> deckOne = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
 
+        Set<Integer> deckTwo = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+
+        int rounds = 50;
+        while (rounds-- > 0) {
+            if (deckOne.isEmpty() || deckTwo.isEmpty()) {
+                break;
+            }
+
+            Iterator<Integer> firstIterator = deckOne.iterator();
+            int firstCard = firstIterator.next();
+            firstIterator.remove();
+
+            Iterator<Integer> secondIterator = deckTwo.iterator();
+            int secondCard = secondIterator.next();
+            secondIterator.remove();
+
+            if (firstCard > secondCard) {
+
+                deckOne.add(firstCard);
+                deckOne.add(secondCard);
+
+            } else if (firstCard < secondCard){
+                deckTwo.add(firstCard);
+                deckTwo.add(secondCard);
+
+            }
+        }
+
+        String output = "Draw!";
+        if (deckOne.size() < deckTwo.size()) {
+            output = "Second player win!";
+        } else if (deckOne.size() > deckTwo.size()) {
+            output = "First player win!";
+        }
+        System.out.println(output);
+    }
+
+    public static int getFirstCard(Set<Integer> cards) {
+        for (Integer card : cards) {
+            return card;
+        }
+        return 0;
     }
 }
