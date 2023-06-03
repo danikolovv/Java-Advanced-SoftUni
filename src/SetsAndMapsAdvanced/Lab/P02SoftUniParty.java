621bb44d1cfb3b098207ad1ef0f344261569f26e
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-06-03 23:12:51 +0300

    Initial commit

--- src/SetsAndMapsAdvanced/Lab/P02SoftUniParty.java
+++ src/SetsAndMapsAdvanced/Lab/P02SoftUniParty.java
@@ -1,13 +1,41 @@
 package SetsAndMapsAdvanced.Lab;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class P02SoftUniParty {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Set<String> guests = new TreeSet<>();
+        Set<String> coming = new LinkedHashSet<>();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("PARTY")) {
+
+            if (input.length() == 8) {
+                guests.add(input);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        while (!input.equals("END")) {
+
+            guests.remove(input);
+            coming.add(input);
+
+            input = scanner.nextLine();
+        }
+
+        System.out.println(guests.size());
+
+        for (String guest : guests) {
+            System.out.println(guest);
+        }
 
     }
 }
---
+++ src/SetsAndMapsAdvanced/Lab/P03VoinaNumberGame.java
@@ -0,0 +1,11 @@
+package SetsAndMapsAdvanced.Lab;
+
+import java.util.Scanner;
+
+public class P03VoinaNumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
