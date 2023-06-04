f20b2fd8afe98ba047c52f750e4f397ea27b3aea
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-06-04 13:04:24 +0300

    Initial commit

---
+++ src/SetsAndMapsAdvanced/Exercise/P04CountSymbols.java
@@ -0,0 +1,25 @@
+package SetsAndMapsAdvanced.Exercise;
+
+import java.util.*;
+
+public class P04CountSymbols {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char[] input = scanner.nextLine().toCharArray();
+
+        Map<Character, Integer> characterIntegerMap = new TreeMap<>();
+
+        for (char c : input) {
+
+            if (!characterIntegerMap.containsKey(c)){
+                characterIntegerMap.put(c, 1);
+            } else {
+                characterIntegerMap.put(c, characterIntegerMap.get(c) + 1);
+            }
+
+        }
+
+        characterIntegerMap.forEach((k, v) -> System.out.printf("%c : %d time/s%n", k, v));
+    }
+}
