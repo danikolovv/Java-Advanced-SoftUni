a133aea5d4b91fa6b6aa0e9254442b5ff6031650
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-06-16 11:58:30 +0300

    Initial commit

--- src/FunctionalProgramming/Lab/P05FilterByAge.java
+++ src/FunctionalProgramming/Lab/P05FilterByAge.java
@@ -6,6 +6,17 @@ public class P05FilterByAge {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int N = Integer.parseInt(scanner.nextLine());
 
+        for (int i = 0; i < N; i++) {
+            String[] tokens = scanner.nextLine().split(", ");
+            String name = tokens[0];
+            int age = Integer.parseInt(tokens[1]);
+            String condition = scanner.nextLine();
+            int ageInteger = Integer.parseInt(scanner.nextLine());
+            String format = scanner.nextLine();
+
+            
+        }
     }
 }
