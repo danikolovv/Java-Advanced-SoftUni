543007d790fb3b3aabfe4cc9cfb5eb5e3d23b2b9
danikolovv <114946180+danikolovv@users.noreply.github.com>
2023-06-16 12:36:07 +0300

    Initial commit

--- src/StacksAndQueues/Exercise/P10Robotics.java
+++ src/StacksAndQueues/Exercise/P10Robotics.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 
 public class P10Robotics {
     public static void main(String[] args) throws IOException {
